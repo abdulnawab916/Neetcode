8269b5a3b3f563811599129ca83a895a268f1209
Abdul Nawab <abdulnawab98@gmail.com>
2024-09-15 12:19:23 -0700

    Added 3sum prob

---
+++ 3SumProblem.py
@@ -0,0 +1,7 @@
+# Problem Type: Two Pointer
+# How does two pointer work?
+# Refresher of two pointer:
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # WE have to find all the possible pairs of numbers
+        # that will sum up to -> '0'
