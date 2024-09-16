5538879ef80b9cb1078ceba42316b35e1cbedde8
Abdul Nawab <abdulnawab98@gmail.com>
2024-09-16 13:23:53 -0700

    linked list implementation

--- DesignALinkedList.py
+++ DesignALinkedList.py
@@ -1,15 +1,27 @@
-# Creating a Linked List (Singly)
 # Note:
 # You are allowed to create a Node class! 
 # Any time that you want to create a linked list, you 
 # HAVE to create a Node class
+
+# things that I fixed:
+# 
+#
+#
+
+# What I learned about Python
+# In Python, when you instantiate an object from a class, you do not
+# use the 'new' keyword. All you do is that you call the
+# class itself with the corresponding parameters
+
+
+
 class Node:
-    def __init__(self, val=0, next=None):
-        self.val = val
+    def __init__(self, value = 0, next = None):
+        self.val = value
         self.next = next
 
 class MyLinkedList:
-    def __init__(self, head, size):
+    def __init__(self, head = None, size= 0):
         # initialize the head to be nothing
         self.head = None
         # there is nothing in the linked list yet
@@ -20,24 +32,32 @@ class MyLinkedList:
         counterVar = 0
         currNode = self.head
         currVal = 0 # init curr value variable
-        while counterVar <= index:
-            currNode = currNode.next
-            currValue = currNode.val
-            counterVar += 1 # increment on next time we go around the loop
-        return currValue
+        # check the case where the index is the thing we are looking for
+        if counterVar == index:
+            return currNode.val # Just returning the value of the node that we are at
+        # If the counterVar is not the index
+        else:
+            # get back the head
+            currNode = self.head
+            # make another counter
+            secondCounter = 0
+            while secondCounter < index and currNode:
+                currNode = currNode.next
+                currVal = currNode.val # val or value for accessing
+                # forgot to iterate through the list by incrementing
+                secondCounter += 1
+        return currVal
 
     def addAtHead(self, val: int) -> None:
-        # Doesn't return anything
         # allocate space for the head by creating a new node
-        newHead = new Node(val, self.head)
+        newHead = Node(val)
         # newHead.next = self.head 
         # This makes sure that the next of this new head is the 'old' head
         self.head = newHead # update the head of the linked list
-    # Tip: Create  tail pointer to account for stuff. But, there could be a diff
-    # Way to do this without making a tail pointer    
+    
     def addAtTail(self, val: int) -> None:
         # allocate memory for a new Node
-        newTailNode = new Node(val)
+        newTailNode = Node(val) # this is how you allocate an object in Python
         # create Curr pointer to traverse
         currPointer = self.head
         # create a counter to keep track of the nodes that we are 
@@ -56,18 +76,19 @@ class MyLinkedList:
         
         # creating a secondCounter variablew for second iteration
         secondCounter = 0
-        while secondCounter <= actualValue:
+        while secondCounter <= actualValue and currPointer:
             currPointer = currPointer.next
         
         # then, we are going to assign the currpointer next to be 
         # the new pointer that we created
-        currPointer.next = newTailNode
-        newTailNode.next = None # make sure tail has nothing after it
+        while currPointer:
+            currPointer.next = newTailNode
+            newTailNode.next = None # make sure tail has nothing after it
 
-    def addAtIndex(self, index: int, val: int) -> None:
+   # def addAtIndex(self, index: int, val: int) -> None:
         
 
-    def deleteAtIndex(self, index: int) -> None:
+    # def deleteAtIndex(self, index: int) -> None:
         
 
 
@@ -78,3 +99,9 @@ class MyLinkedList:
 # obj.addAtTail(val)
 # obj.addAtIndex(index,val)
 # obj.deleteAtIndex(index)
+
+# How to work with test cases
+# ["MyLinkedList","addAtHead", ]
+# You have to make sure that the parameters match with the number, and then you have
+# to make sure that the arrays match up and whatnot
+
